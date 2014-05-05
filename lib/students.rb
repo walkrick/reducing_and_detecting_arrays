74acaafb7491c8438dc87745c4be559dc50c1b20
Mike Gehard <mike.gehard@gmail.com>
2014-05-05 10:41:57 -0600

    Initial commit

---
+++ lib/students.rb
@@ -0,0 +1,11 @@
+class Students
+  def all
+    [
+        {name: "Bob", age: 10},
+        {name: "Sue", age: 12},
+        {name: "Greg", age: 15},
+        {name: "Amanda", age: 8},
+        {name: "Steve", age: 7},
+    ]
+  end
+end
---
+++ spec/lib/students_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+require 'students'
+
+describe Students do
+  it 'can return all students' do
+    students = Students.new
+
+    expected_students = [
+        {name: "Bob", age: 10},
+        {name: "Sue", age: 12},
+        {name: "Greg", age: 15},
+        {name: "Amanda", age: 8},
+        {name: "Steve", age: 7},
+    ]
+
+    expect(students.all).to match_array expected_students
+  end
+end
