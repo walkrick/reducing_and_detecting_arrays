c7d9a8c68473f3c586f9a3bcecfba1b27dd79e27
Mike Gehard <mike.gehard@gmail.com>
2014-05-08 10:33:23 -0600

    In class coding

--- lib/students.rb
+++ lib/students.rb
@@ -5,7 +5,92 @@ class Students
         {name: "Sue", age: 10},
         {name: "Greg", age: 15},
         {name: "Amanda", age: 8},
-        {name: "Steve", age: 21},
+        {name: "Steve", age: 22},
     ]
+  end
+
+  def average_age
+    # all.inject(0) { |sum_of_ages, student| sum_of_ages + student[:age] } / all.length
+    all.reduce(0) { |sum_of_ages, student| sum_of_ages + student[:age] } / all.length
+
+    # sum_of_ages = 0
+    # all.each do |student|
+    #   sum_of_ages = sum_of_ages + student[:age]
+    # end
+    #
+    # sum_of_ages/all.length
+  end
+
+  def name_string
+    all.reduce("") { |accumulator, student| accumulator + student[:name] + " " }.strip
+    # all.map { |student| student[:name] }.join(' ')
+  end
+
+  def find_first_older_than(age)
+    # all.each do |student|
+    #   if student[:age] > age
+    #     return student
+    #   end
+    # end
+
+    all.detect { |student| student[:age] > age }
+  end
+
+  def any_older_than?(age)
+    all.any? { |student| student[:age] > age }
+
+    # all.each do |student|
+    #   if student[:age] > age
+    #     return true
+    #   end
+    # end
+    # false
+  end
+
+  def find_student(student_to_find)
+    all.detect { |student| student == student_to_find }
+
+    # all.each do |student|
+    #   if student == student_to_find
+    #     return student
+    #   end
+    # end
+    # nil
+  end
+
+  def student_present?(student_to_find)
+    all.include?(student_to_find)
+
+    # all.each do |student|
+    #   if student == student_to_find
+    #     return true
+    #   end
+    # end
+    # false
+  end
+
+  def grouped_by_age
+    # This is a little less clear and
+    # may be an anti-pattern depending
+    # on who you talk to
+    all.inject({}) do |acc, student|
+      if  acc[student[:age]] == nil
+        acc[student[:age]] = []
+      end
+      acc[student[:age]] << student[:name]
+      acc
+    end
+
+    # I (MikeG) prefers this solution because it
+    # is a bit more clear in my head
+    # acc = {}
+    #
+    # all.each do |student|
+    #   if  acc[student[:age]] == nil
+    #     acc[student[:age]] = []
+    #   end
+    #   acc[student[:age]] << student[:name]
+    # end
+    # acc
   end
 end
--- spec/lib/students_spec.rb
+++ spec/lib/students_spec.rb
@@ -3,17 +3,65 @@ require 'spec_helper'
 require 'students'
 
 describe Students do
+  let(:students) { Students.new }
   it 'can return all students' do
-    students = Students.new
-
     expected_students = [
         {name: "Bob", age: 10},
         {name: "Sue", age: 10},
         {name: "Greg", age: 15},
         {name: "Amanda", age: 8},
-        {name: "Steve", age: 21},
+        {name: "Steve", age: 22},
     ]
 
     expect(students.all).to match_array expected_students
+  end
+
+  it 'can calculate the average age of the class' do
+    students = Students.new
+
+    expect(students.average_age).to eq 13
+  end
+
+  it 'can return a string of student names' do
+    students = Students.new
+
+    expect(students.name_string).to eq "Bob Sue Greg Amanda Steve"
+  end
+
+  it 'can find a student older than a certain age' do
+    actual = students.find_first_older_than(10)
+
+    expect(actual).to eq({name: "Greg", age: 15})
+  end
+
+  it 'can find out if there are any old enough to drink' do
+    expect(students.any_older_than?(21)).to eq true
+
+    expect(students.any_older_than?(55)).to eq false
+  end
+
+  it 'can find a specific student' do
+    actual = students.find_student({name: "Sue", age: 10})
+    expect(actual).to eq({name: "Sue", age: 10})
+
+    actual = students.find_student({name: "Homer", age: 30})
+    expect(actual).to eq(nil)
+
+  end
+
+  it 'can determine if a specific student is present' do
+    expect(students.student_present?({name: "Sue", age: 10})).to eq true
+
+    expect(students.student_present?({name: "Homer", age: 30})).to eq false
+  end
+
+  it 'can group students by age' do
+    expected = {
+        10 => ["Bob", "Sue"],
+        15 => ["Greg"],
+        8 => ["Amanda"],
+        22 => ["Steve"],
+    }
+    expect(students.grouped_by_age).to eq expected
   end
 end
